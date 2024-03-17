86dfe74654df2698879be83b0a3ed2324c4a82df
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-03-17 23:52:28 +0300

    feat: move logout button to sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Navigation } from '@/components/navigation'
+import { Button } from '@/components/ui/button'
+import { useAuthStore } from '@/stores/auth'
+
+const router = useRouter()
+const authStore = useAuthStore()
+
+async function onLogout() {
+  authStore.logout()
+  await router.push('/login')
+}
 </script>
 
 <template>
   <div
-    class="sidebar flex-col max-w-[240px] w-full gap-1 border-r border-border h-full p-2 grid"
+    class="sidebar flex-col max-w-[240px] w-full gap-1 border-r border-border h-full grid grid-rows-[min-content_1fr]"
   >
-    <ScrollArea class="h-full">
+    <div class="p-2 border-b border-border">
+      <Button
+        v-if="authStore.isAuthenticated"
+        variant="outline"
+        size="icon"
+        @click="onLogout"
+      >
+        <Icon icon="mdi:logout" />
+      </Button>
+    </div>
+    <ScrollArea class="h-full p-2">
       <Navigation />
     </ScrollArea>
   </div>
--- src/components/app-header/AppHeader.vue
+++ src/components/app-header/AppHeader.vue
@@ -1,19 +1,8 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-
-import { Button } from '@/components/ui/button'
 import ThemeToggle from './ThemeToggle.vue'
 import { useAppStore } from '@/stores/app'
-import { useAuthStore } from '@/stores/auth'
 
-const router = useRouter()
 const appStore = useAppStore()
-const authStore = useAuthStore()
-
-async function onLogout() {
-  authStore.logout()
-  await router.push('/login')
-}
 </script>
 
 <template>
@@ -21,14 +10,6 @@ async function onLogout() {
     <div class="text-lg font-bold px-2">{{ appStore.config?.name }}</div>
     <div class="flex items-center gap-2">
       <ThemeToggle />
-      <Button
-        v-if="authStore.isAuthenticated"
-        variant="outline"
-        size="icon"
-        @click="onLogout"
-      >
-        <Icon icon="mdi:logout" />
-      </Button>
     </div>
   </header>
 </template>
