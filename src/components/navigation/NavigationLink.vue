c89794f35d704e098b3a231fd75a29f5b280223a
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-03-25 18:06:45 +0300

    fix(NavigationLink): styles for active-class is not applied

--- src/components/navigation/NavigationLink.vue
+++ src/components/navigation/NavigationLink.vue
@@ -8,7 +8,7 @@ defineProps<{
 <template>
   <router-link
     class="px-4 py-2 pl-6 text-sm hover:text-foreground text-muted-foreground inline-flex transition-colors"
-    active-class="text-foreground"
+    active-class="!text-foreground"
     :to="to"
   >
     {{ title }}
