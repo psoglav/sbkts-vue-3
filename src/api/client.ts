92633597bd6bfc4b554ab26d3641c12739a3a941
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-03-25 17:14:52 +0300

    feat(api): use interceptor for authorizing requests

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,11 +1,20 @@
 import axios from 'axios'
+import { useAuthStore } from '@/stores/auth'
 
-export default axios.create({
+const client = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
       ? '/api'
       : 'http://localhost:8080/api',
   headers: {
     'Content-type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('user')}`,
   },
 })
+
+client.interceptors.request.use(function (config) {
+  config.headers.Authorization = `Bearer ${useAuthStore().user?.token}`
+  return config
+})
+
+export default client
